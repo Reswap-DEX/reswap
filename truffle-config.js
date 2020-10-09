b6cc5c082aa74ee05b1a91dfc6068232cab310a2
akshaynexus <akshaycm@hotmail.com>
2020-10-09 18:35:44 +0300

    Add networks for bsc mainnet,testnet,misc adds

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const { mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
     compilers: {
         solc: {
@@ -20,5 +23,26 @@ module.exports = {
             showTimeSpent: true,
             excludeContracts: ['Migrations', 'mocks']
         }
-    }
+    },
+    networks: {
+        development: {
+          provider: () => new HDWalletProvider(mnemonic, "https://data-seed-prebsc-1-s1.binance.org:8545"),
+          network_id: 97, // match any network
+          websockets: false
+        },
+        live: {
+          host: "bsc-dataseed.binance.org", // Random IP for example purposes (do not use)
+          port: 80,
+          network_id: 56,        // Ethereum public network
+          // optional config values:
+          // gas
+          // gasPrice
+          // from - default address to use for any transaction Truffle makes during migrations
+          // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+          //          - function that returns a web3 provider instance (see below.)
+          //          - if specified, host and port are ignored.
+          // skipDryRun: - true if you don't want to test run the migration locally before the actual migration (default is false)
+          // timeoutBlocks: - if a transaction is not mined, keep waiting for this number of blocks (default is 50)
+        }
+      }
 };
